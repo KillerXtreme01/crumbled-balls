0939c082db5fd9407d707546ac29a28522fa681b
KillerXtreme01 <77969044+KillerXtreme01@users.noreply.github.com>
2021-04-25 15:23:22 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper
+{
+    constructor(x,y,r){
+ var options = {
+    isStatic : false,
+    restitution : 0.3,
+    friction : 0.5,
+    density : 1.2
+}
+this.r = r
+
+this.image = loadImage("paper.png")
+this.body=Bodies.circle(x, y, r, options)
+World.add(world,this.body)
+    }    
+    display()
+    {
+    var pos = this.body.position;
+    push()
+    translate(pos.x, pos.y)
+    rectMode(CENTER)
+    fill(0, 255, 191)
+    imageMode(CENTER);
+    image(this.image, 0, 0, 2*this.r, 2*this.r)
+    pop()
+
+    }
+}
